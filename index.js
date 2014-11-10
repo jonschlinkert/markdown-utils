00f89dac5732de5c1953788ab6b84437b7102de3
jonschlinkert <dev@sellside.com>
2014-11-10 01:50:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*!
+ * markdown-utils <https://github.com/jonschlinkert/markdown-utils>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = require('./lib');
